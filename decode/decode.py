474ce613b6a49fbdd1c0cdfac0257f9799a1f2a5
GuiHenriques <gui13ghc@gmail.com>
2024-09-27 15:00:58 -0300

    commando line data input

--- decode/decode.py
+++ decode/decode.py
@@ -1,10 +1,11 @@
 import json
 import base64
 import os
+import sys
 
 def convert_base64_to_audio(json_file, output_dir):
     os.makedirs(output_dir, exist_ok=True)
-    
+
     # Load the JSON file
     with open(json_file, 'r') as f:
         data = json.load(f)
@@ -22,7 +23,15 @@ def convert_base64_to_audio(json_file, output_dir):
             with open(output_file, 'wb') as audio_file:
                 audio_file.write(audio_data)
 
-# Example usage
-json_file = 'data (6).json'
-output_dir = 'output'
-convert_base64_to_audio(json_file, output_dir)
+if __name__ == "__main__":
+    # Check if the JSON file was passed as a command-line argument
+    if len(sys.argv) != 2:
+        print("Usage: python3 decode.py <json_file>")
+        sys.exit(1)
+
+    # Get the JSON file from command-line argument
+    json_file = sys.argv[1]
+    output_dir = f"{json_file[:5]}_output"
+
+    # Call the function to convert base64 to audio
+    convert_base64_to_audio(json_file, output_dir)
